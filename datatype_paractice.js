49c490ed471b5a908176f9e42ea67101f05892bb
Deepak Das <157786118+Deepakdas8970@users.noreply.github.com>
2024-02-07 09:33:53 +0530

    Add files via upload

---
+++ datatype_paractice.js
@@ -0,0 +1,25 @@
+/*
+practice questions:-
+X 10 + "20"
+
+X 9 "5"
+
+X 'Java" + "Script" 11
+
+X
+
+11 11 + 11
+
+11 X + 0
+
+X "vinod" "thapa"
+
+X true true
+
+X true false
+
+X false + true
+
+X false - true*/
+
+console.log(10 + "20");
